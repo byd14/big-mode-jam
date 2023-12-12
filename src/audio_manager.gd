ae379ab140bbe067717e540a01a77889473d25bf
byd14 <wrenchtop@gmail.com>
2023-12-12 14:30:30 +0400

    added volume and pitch control for audio manager

--- src/audio_manager.gd
+++ src/audio_manager.gd
@@ -1,17 +1,21 @@
 extends Node
 
-func play(sound : AudioStream) -> AudioStreamPlayer:
+func play(sound : AudioStream, volume := 0, pitch := 1.0) -> AudioStreamPlayer:
 	var i := AudioStreamPlayer.new()
 	i.stream = sound
+	i.volume_db = volume
+	i.pitch_scale = pitch
 	i.finished.connect(free_player.bind(i))
 	add_child(i)
 	i.play()
 	return i
 
-func play_2d(sound : AudioStream, position : Vector2) -> AudioStreamPlayer2D:
+func play_2d(sound : AudioStream, position : Vector2, volume := 0, pitch := 1.0) -> AudioStreamPlayer2D:
 	var i := AudioStreamPlayer2D.new()
-	i.global_position = position
 	i.stream = sound
+	i.volume_db = volume
+	i.pitch_scale = pitch
+	i.global_position = position
 	i.finished.connect(free_player.bind(i))
 	add_child(i)
 	i.play()
